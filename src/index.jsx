a8e9a948fa70ecbab35c1ee103ad1190dc087080
James Abels <jamesrabels@gmail.com>
2020-01-25 16:47:25 -0500

    Add google analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Analytics from 'analytics'
+import googleAnalytics from '@analytics/google-analytics'
+
 import './index.scss';
 import 'leaflet-css';
 
 import App from './app';
 import ApplicationStore from './ApplicationStore';
 
-let appStore = new ApplicationStore();
+const appStore = new ApplicationStore();
+
+const analytics = Analytics({
+    app: 'EXIF-Hound',
+    version: 0,
+    plugins: [
+      googleAnalytics({
+        trackingId: 'UA-125044509-1',
+      })
+    ]
+})
+
+analytics.page()
 
 ReactDOM.render(
     <App store={appStore} />,
